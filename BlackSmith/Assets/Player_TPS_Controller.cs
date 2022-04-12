3a23d7abc3341d46ee022282fadbae12690f1e63
Pelome <pgbayard@gmail.com>
2022-04-12 14:23:40 +0200

    hop

---
+++ BlackSmith/Assets/Player_TPS_Controller.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_TPS_Controller : MonoBehaviour
+{
+    public float speed = 5;
+    public float jumpForce = 5;
+    public float dashSpeed = 5;
+    private bool facingRight = true;
+    private Rigidbody2D rb;
+    public bool isGrounded;
+    public LayerMask groundLayer;
+    //private Collision coll
+    public float collisionRadius = 0.25f;
+    public Vector2 bottomOffset, rightOffset, leftOffset;
+
+    void Start()
+    {
+        //coll = GetComponent<Collision>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle((Vector2)transform.position + bottomOffset, collisionRadius, groundLayer);
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector2 dir = new Vector2(x, y);
+
+        Walk(dir);
+        //player movement 
+        
+        //player dash
+        if(Input.GetButtonDown("Dash"))
+        {
+            Dash(10,10);
+        }
+        
+        //player jump
+        if(Input.GetButtonDown("Jump")&& !isGrounded)
+        {
+            Jump();
+        }
+
+    }
+
+    private void Walk(Vector2 dir)
+    {
+        rb.velocity = (new Vector2(dir.x * speed, rb.velocity.y));
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, 0);
+        rb.velocity += Vector2.up * jumpForce;
+    }
+
+    private void Dash(float x, float y)
+    {
+        rb.velocity = Vector2.zero;
+        Vector2 dir = new Vector2(x, y);
+
+        rb.velocity += dir.normalized * dashSpeed;
+    }
+
+    private void flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
+
