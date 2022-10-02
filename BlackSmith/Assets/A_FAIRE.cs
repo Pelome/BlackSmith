b83b40dea3ce84658aa300512b73f94651edba29
Pelome <pgbayard@gmail.com>
2022-10-02 10:40:16 +0200

    resetOrigin

---
+++ BlackSmith/Assets/A_FAIRE.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class A_FAIRE : MonoBehaviour
+{
+
+
+// refaire origine.y a 0 pour tous les objets
+
+// positionnement du personnage en 0:0vertical, les deplacement le bougent
+//rabaisser un peu les jambes et corriger les animations
+//retravailler l'animation de marche
+//vérifier que cela fonctionne sur tout les persos
+//verifier que les animations des parties spéciales (meche/barbe/bras) fonctionnenet si le personnage n'en a pas
+
+
+//son de certaines animations HS
+//infdicateur de succès a chaque etape de forgeage réussi
+//indicateur d'echec
+//usivi des étapes de mission
+
+//rabaisser un peu les jambes et corriger les animations
+
+//le corriger le spawner de mission
+
+//rendre actif le positionnement dans le coffre 
+
+// faire un étalage
+
+//verifier que les animations se cassent quand on sort (ou empecher de sortir)
+
+//pouvoir prendre une mission
+
+//voleur, pouvoir le frapper
+
+
+
+//metttre en place le systeme de monnaie
+
+//mettre en place un apprenti.
+//payer pour monter sa capacité.
+
+//améliorer sa forge
+
+//cycle jour/nuit avec voleurs
+}
